251997bde5842e8c0058b4a71d9fc0890a48521a
SRS0301 <sreyas00sunil@gmail.com>
2023-05-23 17:17:15 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Home from './pages/home'
+import Counter from './pages/Counter'
+export default function App() {
+  return (
+    <div>
+      <Home/>
+      <Counter/>
+    </div>
+  )
+}
